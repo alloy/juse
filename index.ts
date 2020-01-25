4f45353e91b1967e03b46e4c44b85addb6fce8b2
Eloy Durán <eloy.de.enige@gmail.com>
2020-01-25 23:54:59 +0100

    Transform files with registered extnames

--- index.ts
+++ index.ts
@@ -1,33 +1,63 @@
-import Runtime from "jest-runtime"
 import { Config } from '@jest/types'
-import { HasteMap } from "jest-haste-map/build/types"
-import { JestEnvironment } from '@jest/environment'
 import { readConfig } from "jest-config"
+import { ScriptTransformer } from '@jest/transform'
 import realpathNative from "realpath-native"
+import stripBom from "strip-bom"
 import yargs from "yargs"
-import os from "os"
+import fs from "fs"
+import path from "path"
 
-async function init() {
+class Runtime {
+    private _scriptTransformer: ScriptTransformer
+    private _extensions: string[]
+
+    constructor(config: Config.ProjectConfig) {
+        this._extensions = ["json", ...config.moduleFileExtensions].map(e => `.${e}`)
+        this._scriptTransformer = new ScriptTransformer(config)
+    }
+
+    transformFile(filename: string) {
+        if (!this._extensions.includes(path.extname(filename))) {
+            console.log(`Unhandled extname: ${filename}`)
+            return null
+        }
+
+        const text = stripBom(fs.readFileSync(filename, 'utf8'))
+    
+        if (path.extname(filename) === ".json") {
+            return this._scriptTransformer.transformJson(
+                filename,
+                {} as any, // this._getFullTransformationOptions(options),
+                text
+            )
+        } else {
+            const transformedFile = this._scriptTransformer.transform(
+                filename,
+                {} as any, // this._getFullTransformationOptions(options),
+                text // this._cacheFS[filename]
+            )
+            return transformedFile.code
+        }
+    }
+}
+
+async function createRuntime() {
     const root = realpathNative.sync(process.cwd());
     const options = await readConfig(yargs.argv, root);
+    return new Runtime(options.projectConfig)
+}
 
-    const config: Config.ProjectConfig = options.projectConfig
+async function init() {
+    try {
+        const runtime = await createRuntime()
 
-    console.log(config)
-
-    const Environment = require(config.testEnvironment);
-    const environment: JestEnvironment = new Environment(config)
-  
-    const hasteMap: HasteMap = await Runtime.createContext(config, {
-        maxWorkers: Math.max(os.cpus().length - 1, 1),
-        watchman: options.globalConfig.watchman
-    })
-
-    const cacheFS = undefined // TODO: Should this be persisted?
-
-    const resolver = Runtime.createResolver(config, hasteMap.moduleMap)
-    const runtime = new Runtime(config, environment, resolver, cacheFS)
-    console.log(runtime)
+        console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/yarn.lock"))
+        console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/package.json"))
+        console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/src/lib/AppRegistry.tsx"))
+    }
+    catch(e) {
+        console.error(e)
+    }
 }
 
 init()
