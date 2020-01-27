2a4480c2f42c49cba40769ab255d23dd0984e257
Eloy Durán <eloy.de.enige@gmail.com>
2020-01-27 22:42:17 +0100

    First working version

--- index.ts
+++ index.ts
@@ -2,58 +2,107 @@ import { Config } from '@jest/types'
 import { readConfig } from "jest-config"
 import { ScriptTransformer } from '@jest/transform'
 import realpathNative from "realpath-native"
-import stripBom from "strip-bom"
+// import stripBom from "strip-bom"
 import yargs from "yargs"
 import fs from "fs"
 import path from "path"
+import Fuse from "fuse-native"
 
 class Runtime {
     private _scriptTransformer: ScriptTransformer
     private _extensions: string[]
 
     constructor(config: Config.ProjectConfig) {
-        this._extensions = ["json", ...config.moduleFileExtensions].map(e => `.${e}`)
+        this._extensions = config.moduleFileExtensions.map(e => `.${e}`)
         this._scriptTransformer = new ScriptTransformer(config)
     }
 
     transformFile(filename: string) {
         if (!this._extensions.includes(path.extname(filename))) {
-            console.log(`Unhandled extname: ${filename}`)
             return null
         }
 
-        const text = stripBom(fs.readFileSync(filename, 'utf8'))
-    
-        if (path.extname(filename) === ".json") {
-            return this._scriptTransformer.transformJson(
-                filename,
-                {} as any, // this._getFullTransformationOptions(options),
-                text
-            )
-        } else {
-            const transformedFile = this._scriptTransformer.transform(
-                filename,
-                {} as any, // this._getFullTransformationOptions(options),
-                text // this._cacheFS[filename]
-            )
-            return transformedFile.code
-        }
+        const transformedFile = this._scriptTransformer.transform(
+            filename,
+            {} as any, // TODO: do we need to provide options?
+            null as any // externally provided cached content
+        )
+        return transformedFile.code
     }
 }
 
-async function createRuntime() {
-    const root = realpathNative.sync(process.cwd());
-    const options = await readConfig(yargs.argv, root);
+async function createRuntime(root: string) {
+    const options = await readConfig(yargs.argv, root)
     return new Runtime(options.projectConfig)
 }
 
 async function init() {
     try {
-        const runtime = await createRuntime()
+        // console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/yarn.lock"))
+        // console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/package.json"))
+        // console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/src/lib/AppRegistry.tsx"))
+        // console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/src/lib/AppRegistry.tsx"))
 
-        console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/yarn.lock"))
-        console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/package.json"))
-        console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/src/lib/AppRegistry.tsx"))
+        const mnt = process.argv[2]
+        if (!mnt) {
+            throw new Error("Require mount point")
+        }
+
+        const root = realpathNative.sync(process.cwd())
+        const runtime = await createRuntime(root)
+
+        const fuse = new Fuse(mnt, {
+            readdir(readPath, cb) {
+              readPath = path.join(root, readPath)
+              // console.log(readPath)
+              fs.readdir(readPath, (err, files) => cb((err && err.errno) || 0, files))
+            },
+            getattr(readPath, cb) {
+              readPath = path.join(root, readPath)
+              fs.stat(readPath, (err, stat) => {
+                if (err) {
+                  cb(err.errno!)
+                } else {
+                  // TODO: This should really run in parallel with fs.stat, but the default cache of
+                  //       ScriptTransformer uses readSync to read the file.
+                  const code = runtime.transformFile(readPath)
+                  if (code) {
+                    cb(0, { ...stat, size: code.length })
+                  } else {
+                    cb(0, stat)
+                  }
+                }
+              })
+            },
+            open(readPath, flags, cb) {
+              readPath = path.join(root, readPath)
+              const code = runtime.transformFile(readPath)
+              if (code) {
+                cb(0, 0)
+              } else {
+                fs.open(readPath, flags, (err, fd) => cb((err && err.errno) || 0, fd))
+              }
+            },
+            release(_readPath, fd, cb) {
+              if (fd !== 0) {
+                fs.close(fd, () => cb(0))
+              }
+            },
+            read(readPath, fd, buf, len, pos, cb) {
+              if (fd !== 0) {
+                fs.read(fd, buf, pos, len, pos, (_err, bytesRead) => cb(bytesRead))
+              } else {
+                readPath = path.join(root, readPath)
+                const data = runtime.transformFile(readPath)!.slice(pos, pos + len)
+                console.log({ readPath, pos, len })
+                buf.write(data)
+                cb(data.length)
+              }
+            }
+          }, { debug: true })
+          fuse.mount(function (err) {
+            console.error(err)
+          })
     }
     catch(e) {
         console.error(e)
