22bbc80373c4d64cfe66e0cbb0d8e4319472702f
Eloy Durán <eloy.de.enige@gmail.com>
2020-01-28 00:11:02 +0100

    Cleanup

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
+/**
+ * TODO:
+ * - Read source files non-block, unlike what the ScriptTransformer does. Unsure what Jest offers.
+ * - Fill build cache ahead of time. Unsure what Jest offers.
+ * - Use Jest’s platform aware resolver to check if files should be transformed.
+ * - Rename file extnames to .js
+ */
+
 import { Config } from '@jest/types'
 import { readConfig } from "jest-config"
 import { ScriptTransformer } from '@jest/transform'
@@ -18,16 +26,11 @@ class Runtime {
     }
 
     transformFile(filename: string) {
-        // TODO: Jest's resolver wouldn’t load this, as its list of extensions includes e.g. .ios.js and thus all extensions can be matched
+        // TODO: Temp hack that would go away if Jest's resolver were used and whole extnames would be matched.
         if (filename.endsWith(".d.ts") || !this._extensions.includes(path.extname(filename))) {
             return null
         }
-
-        const transformedFile = this._scriptTransformer.transform(
-            filename,
-            {} as any, // TODO: do we need to provide options?
-            null as any // externally provided cached content
-        )
+        const transformedFile = this._scriptTransformer.transform(filename, {} as any, null as any)
         return transformedFile.code
     }
 }
@@ -39,11 +42,6 @@ async function createRuntime(root: string) {
 
 async function init() {
     try {
-        // console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/yarn.lock"))
-        // console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/package.json"))
-        // console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/src/lib/AppRegistry.tsx"))
-        // console.log(runtime.transformFile("/Users/eloy/Code/Artsy/emission/src/lib/AppRegistry.tsx"))
-
         const mnt = process.argv[2]
         if (!mnt) {
             throw new Error("Require mount point")
@@ -55,7 +53,6 @@ async function init() {
         const fuse = new Fuse(mnt, {
             readdir(readPath, cb) {
               readPath = path.join(root, readPath)
-              // console.log(readPath)
               fs.readdir(readPath, (err, files) => cb((err && err.errno) || 0, files))
             },
             getattr(readPath, cb) {
@@ -64,8 +61,6 @@ async function init() {
                 if (err) {
                   return cb(err.errno!)
                 } else if (!stat.isDirectory()) {
-                  // TODO: This should really run in parallel with fs.stat, but the default cache of
-                  //       ScriptTransformer uses readSync to read the file.
                   const code = runtime.transformFile(readPath)
                   if (code) {
                     return cb(0, { ...stat, size: code.length })
